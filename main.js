12b820c52a21b9e2057ffe58dbb3f9a223f1ed52
zhouyichen <zhouyichen123@gmail.com>
2015-12-02 16:15:49 +0800

    Convert input into quaternions

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ function get_and_validate(id) {
 	return parseFloat($(id).val());
 }
 
+function getMagnitude(axis_x, axis_y, axis_z) {
+	return Math.sqrt(axis_x * axis_x + axis_y * axis_y + axis_z * axis_z);
+}
+
+function getRotationQuat(axis_x, axis_y, axis_z, angle) {
+	angle = angle/2;
+	var cos = Math.cos(angle);
+	var sin = Math.sin(angle);
+	var magnitude = getMagnitude(axis_x, axis_y, axis_z);
+	return [
+		cos, 
+		sin * axis_x / magnitude, 
+		sin * axis_y / magnitude, 
+		sin * axis_z / magnitude 
+		]
+}
+
+function getQuatFromPoint(point_x, point_y, point_z) {
+	return [0, point_x, point_y, point_z];
+}
+
 $(function() {
 	$('#calculate').mousedown(function () {
 		var axis_x = get_and_validate('#axis-x');
@@ -15,7 +36,9 @@ $(function() {
 		var point_y = get_and_validate('#point-y');
 		var point_z = get_and_validate('#point-z');
 
-		console.log(axis_x, point_x);
+		var rotation_q = getRotationQuat(axis_x, axis_y, axis_z, angle);
+		var point_q = getQuatFromPoint(point_x, point_y, point_z);
+		console.log(rotation_q, point_q);
 
 	});
 });
